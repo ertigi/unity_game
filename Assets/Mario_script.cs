a58053d5c096dc0a01a0dc50972e2505eb9e36c5
profbor <38473259+profbor@users.noreply.github.com>
2018-06-04 12:31:31 +0300

    Add files via upload

---
+++ Assets/Mario_script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mario_script : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public float jumpSpeed = 0.5f;
+    public LayerMask groundLayer;
+
+    private Animator marioAnimator;
+    private Transform gCheck;
+    private float scaleX = 1.0f;
+    private float scaleY = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+        marioAnimator = GetComponent<Animator>();
+        gCheck = transform.Find("GCheck");
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float mSpeed = Input.GetAxis("Horizontal");
+        marioAnimator.SetFloat("Speed", Mathf.Abs(mSpeed));
+        bool isTouched = Physics2D.OverlapPoint(gCheck.position, groundLayer);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (isTouched)
+            {
+                GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpSpeed, ForceMode2D.Force);
+                isTouched = false;
+            }
+        }
+        marioAnimator.SetBool("isTouched", isTouched);
+
+        if (mSpeed > 0)
+        {
+            transform.localScale = new Vector2(scaleX, scaleY);
+        }
+		else if (mSpeed < 0){
+            transform.localScale = new Vector2(-scaleX, scaleY);
+        }
+
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(mSpeed * speed, GetComponent<Rigidbody2D>().velocity.y);
+        
+    }
+}
